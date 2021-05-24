ffafd9782b6843656ff74bc9915a6b1b348e19c7
Troy Freed <tfreed94@gmail.com>
2021-05-24 19:11:41 -0400

    formatted doc

--- models/productTag.js
+++ models/productTag.js
@@ -1,21 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
+      allowNull: false,
       autoIncrement: true
 
     },
 
     product_id: {
       type: DataTypes.INTEGER,
-      references:{
+      references: {
         model: 'Product',
         key: 'id',
       }
